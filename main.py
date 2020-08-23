e7ce8068ac631f3e533b05a596a57ae4d30748c1
Andre Goranico Samosir <abrammarba@gmail.com>
2020-08-23 07:45:52 +0700

    Update main.py.

--- main.py
+++ main.py
@@ -9,12 +9,56 @@ import requests
 from bs4 import BeautifulSoup
 import xlsxwriter
 
+
 TIMEOUT = 20
 LOOP = 1
 GENDER = 'Female'
 
-driver = webdriver.Chrome(executable_path=os.path.abspath('chromedriver.exe'))
-driver.get('https://www.rolexrankings.com/rankings/2020-08-10')
+
+def load_records(lr_loop, lr_wait, lr_window_height_half):
+    # Loading all records of players.
+    for index_button in range(lr_loop):
+        try:
+            button = lr_wait.until(ec.element_to_be_clickable((By.XPATH, '/html/body/app/main/div[2]/div[4]/button')))
+
+            element_coordinate_y = button.location['y']
+            driver.execute_script("window.scrollTo(0, %d)" % (element_coordinate_y - lr_window_height_half))
+            # time.sleep(1)
+            button.click()
+            time.sleep(1)
+        except TimeoutException:
+            break
+
+
+def make_soup(ms_url):
+    # Make soups of each url.
+    response = requests.get(ms_url)
+    data = response.text
+    ms_soup = BeautifulSoup(data, 'html.parser')
+    return ms_soup
+
+
+def selenium_gets_urls(sgu_driver):
+    # Finding and saving urls.
+    sgu_elements_url = sgu_driver.find_elements_by_css_selector("td._2Dx28 [href]")
+    sgu_urls = []
+    for sgu_element_url in sgu_elements_url:
+        urls.append(sgu_element_url.get_attribute('href'))
+    return sgu_urls
+
+
+def xlsxwriter_write(xw_items, xw_index_row, xw_worksheet, xw_start, xw_format):
+    for xw_counter_col, xw_item in enumerate(xw_items, xw_start):
+        xw_worksheet.write(xw_index_row, xw_counter_col, xw_item, xw_format)
+
+
+def make_driver_chrome():
+    mdc_driver = webdriver.Chrome(executable_path=os.path.abspath('chromedriver.exe'))
+    mdc_driver.get('https://www.rolexrankings.com/rankings/2020-08-10')
+    return mdc_driver
+
+
+driver = make_driver_chrome()
 
 wait = WebDriverWait(driver, TIMEOUT)
 
@@ -23,23 +67,11 @@ window_height = window_size['height']
 window_height_half = int(window_height / 2)
 
 # Loading all records of players.
-for index_button in range(LOOP):
-    try:
-        button = wait.until(ec.element_to_be_clickable((By.XPATH, '/html/body/app/main/div[2]/div[4]/button')))
-
-        element_coordinate_y = button.location['y']
-        driver.execute_script("window.scrollTo(0, %d)" % (element_coordinate_y - window_height_half))
-        # time.sleep(1)
-        button.click()
-        time.sleep(1)
-    except TimeoutException:
-        break
+load_records(LOOP, wait, window_height_half)
 
 # Finding and saving urls.
-elements_url = driver.find_elements_by_css_selector("td._2Dx28 [href]")
 urls = []
-for element_url in elements_url:
-    urls.append(element_url.get_attribute('href'))
+url = selenium_gets_urls(driver)
 
 col_headers = ('Player ID', 'Player Name', 'Last name', 'Other Name', 'Gender', 'Year', 'Events Played', '1st',
                '2nd', '3rd', '4th-10th', 'MC', 'Year End Rank')
@@ -48,18 +80,14 @@ workbook = xlsxwriter.Workbook('data.xlsx')
 worksheet = workbook.add_worksheet()
 bold = workbook.add_format({'bold': True})
 index_row = 0
-# index_col = 0
 
 # Write the headers.
-for counter_col, col_header in enumerate(col_headers):
-    worksheet.write(index_row, counter_col, col_header, bold)
+xlsxwriter_write(col_headers, index_row, worksheet, 0, bold)
 
 # For each record, find and save the data.
 for index_urls, url in enumerate(urls):
-    # Souping each url.
-    response = requests.get(url)
-    data = response.text
-    soup = BeautifulSoup(data, 'html.parser')
+    # Make soups of each url.
+    soup = make_soup(url)
 
     # Monitor its progress.
     print(index_urls + 1)
@@ -95,13 +123,17 @@ for index_urls, url in enumerate(urls):
             index_row += 1
 
             # Write the values of the first 5 columns.
-            for counter_col, value_col in enumerate(value_cols):
-                worksheet.write(index_row, counter_col, value_col)
+            xlsxwriter_write(value_cols, index_row, worksheet, 0, None)
 
-            columns = row.find_all('td')
+            column_urls = row.find_all('td')
+            columns = []
+            # Get the content as text from each column.
+            for column_url in column_urls:
+                column_text = column_url.text
+                columns.append(column_text)
+
             # Write the last values of other columns from the 6th.
             # It's 5 since start at 0.
-            for counter_col, column in enumerate(columns, 5):
-                worksheet.write(index_row, counter_col, column.text)
+            xlsxwriter_write(columns, index_row, worksheet, 5, None)
     break
 workbook.close()
